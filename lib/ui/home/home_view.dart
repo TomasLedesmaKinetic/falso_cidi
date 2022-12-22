7f6c5e12a363f79762d8bb2b43ec3b787b0abf28
TomasLedesmaExtrados <tomas.ledesma@extrados.com.ar>
2022-12-22 15:43:00 -0300

    navigationBar

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -11,6 +11,14 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -33,43 +41,73 @@ class _HomeViewState extends State<HomeView> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          currentIndex: _selectedIndex, //New
+          onTap: _onItemTapped,
           backgroundColor: Colors.purple[100],
           items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home_filled), label: 'Home'),
+            (BottomNavigationBarItem(
+                icon: Icon(Icons.home_filled), label: 'Home')),
             BottomNavigationBarItem(
                 icon: Icon(Icons.file_copy_rounded), label: 'Documentos'),
             BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Cuenta'),
           ]),
-      body: Center(
-        child: SizedBox(
-          height: size.height * 0.9,
-          width: size.width * 0.9,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: size.height * 0.02),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Icon(
-                      Icons.person_rounded,
-                      color: Colors.white,
-                    ),
-                    Column(
-                      children: const [
-                        Text('Nombre y Apellido',
-                            style: TextStyle(color: Colors.white)),
-                        Text('Nivel x', style: TextStyle(color: Colors.white)),
-                      ],
+      body: _selectedIndex == 0
+          ? Center(
+              child: SizedBox(
+                height: size.height * 0.9,
+                width: size.width * 0.9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: size.height * 0.02),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Icon(
+                            Icons.person_rounded,
+                            color: Colors.white,
+                          ),
+                          Column(
+                            children: const [
+                              Text('Nombre y Apellido',
+                                  style: TextStyle(color: Colors.white)),
+                              Text('Nivel x',
+                                  style: TextStyle(color: Colors.white)),
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: size.width * 0.05,
+                                  vertical: size.height * 0.01),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  Icon(Icons.add),
+                                  Text('Subir Nivel'),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                     Container(
                       margin:
                           EdgeInsets.symmetric(vertical: size.height * 0.02),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
+                        color: Colors.purple[100],
                       ),
                       child: Padding(
                         padding: EdgeInsets.symmetric(
@@ -78,153 +116,150 @@ class _HomeViewState extends State<HomeView> {
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: const [
-                            Icon(Icons.add),
-                            Text('Subir Nivel'),
+                            Text('Que estas buscando?'),
+                            Icon(Icons.search_rounded)
                           ],
                         ),
                       ),
                     ),
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: size.height * 0.02),
+                      child: GridView(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                        ),
+                        shrinkWrap: true,
+                        children: [
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                          Container(
+                            height: size.height * 0.01,
+                            margin: EdgeInsets.symmetric(
+                                vertical: size.height * 0.03,
+                                horizontal: size.width * 0.02),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.purple[300],
+                            ),
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.more_horiz_rounded)),
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: size.height * 0.02),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.purple[100],
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: size.width * 0.05,
-                      vertical: size.height * 0.01),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text('Que estas buscando?'),
-                      Icon(Icons.search_rounded)
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: size.height * 0.02),
-                child: GridView(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4,
-                  ),
-                  shrinkWrap: true,
-                  children: [
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                    Container(
-                      height: size.height * 0.01,
-                      margin: EdgeInsets.symmetric(
-                          vertical: size.height * 0.03,
-                          horizontal: size.width * 0.02),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.purple[300],
-                      ),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.more_horiz_rounded)),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+            )
+          : _selectedIndex == 1
+              ? documentosBody()
+              : _selectedIndex == 2
+                  ? cuentaBody()
+                  : Container(),
+    );
+  }
+
+  Widget documentosBody() {
+    return const Center(
+      child: Text('Documentos'),
+    );
+  }
+
+  Widget cuentaBody() {
+    return const Center(
+      child: Text('Cuenta'),
     );
   }
 }
